61cc8fe45a5cf515e656841106373ddb8de71ac7
Oisin Hurley <oisin.hurley@gmail.com>
2014-11-03 08:19:47 +0000

    echo server

--- server.rb
+++ server.rb
@@ -2,7 +2,7 @@ require 'sinatra/base'
 require 'json'
 
 class Server < Sinatra::Base
-  post '/event' do
+  post '/echo' do
     payload = JSON.parse(request.body.read)
     "Hello event #{payload}"
   end
